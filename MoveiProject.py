4ace3099761fdb7f9050f50ca851333c6a36bfac
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-11-15 21:07:41 +0100

    reading and validating data

--- MoveiProject.py
+++ MoveiProject.py
@@ -14,3 +14,12 @@ from matplotlib.pyplot import figure
 matplotlib.rcParams['figure.figsize']=(12,8)# it will adjust the configuration of the plots that will be created
 
 # Read in data
+df = pd.read_csv("movies.csv")
+
+# Displaying the data
+df.head()
+
+#Checking if there is any invalid or null data
+for col in df.columns:
+    pct_missing = np.mean(df[col].isnull())
+    print('{} - {}%'.format(col, round(pct_missing*100)))
