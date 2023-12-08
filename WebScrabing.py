a2d2fb8edd9274fbabcf496cff93c51ad09e758e
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-12-08 21:42:03 +0100

    Function for finding surname column

--- WebScrabing.py
+++ WebScrabing.py
@@ -10,3 +10,14 @@ def page_name(soup):
   h2= soup.find("h2)
   if h2 is not None:
           return h2.text
+
+
+def page_suraname(soup)
+      th= soup.find("td", text="McGrath")
+      if th is None:
+       th= soup.find("td", text="mcGrath")
+      if th is None:
+      th = soup.find("td", text="Mcgrath)
+     if th is not None:  
+      age = th.findNext("th").text
+      return age
